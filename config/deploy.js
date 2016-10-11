7357c922e6e7ddc41af1e22fd087ece638a06319
dev <dev@Zohairs-MacBook-Pro.local>
2016-10-11 11:51:34 -0700

    deploy updated

--- config/deploy.js
+++ config/deploy.js
@@ -9,6 +9,12 @@ module.exports = function(deployTarget) {
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
     // configure other plugins for development deploy target here
+    ENV.s3 = {
+      accessKeyId: 'AKIAIB36XRE6AR3VEHPA',
+      secretAccessKey: '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',
+      bucket: 'deposit-fund',
+      region: 'us-west-2'
+    };
   }
 
   if (deployTarget === 'staging') {
@@ -19,6 +25,12 @@ module.exports = function(deployTarget) {
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
     // configure other plugins for production deploy target here
+    ENV.s3 = {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      bucket: 'my-ember-app',
+      region: 'us-east-1'
+    };
   }
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
